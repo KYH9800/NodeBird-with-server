16b0dcfc873c1e29958a8ebc9a85490cae2b1d3f
고윤혁 <sksgur3217@gmail.com>
2021-10-09 18:38:21 +0900

    implementing-Post, using onToggleClick

---
+++ prepare/front/components/PostImages.js
@@ -0,0 +1,17 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const PostImages = ({ images }) => {
+  // todo
+  return (
+    <>
+      <div>구현 중...</div>
+    </>
+  );
+};
+
+PostImages.PropTypes = {
+  images: PropTypes.arrayOf(PropTypes.object),
+};
+
+export default PostImages;
