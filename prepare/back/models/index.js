23381314b2b6924cbde3490d18941e027dfc9094
고윤혁 <sksgur3217@gmail.com>
2021-10-31 16:42:57 +0900

    completed connect sequelize, MySQL

---
+++ prepare/back/models/index.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize');
+const env = process.env.NODE_ENV || 'development'; // 환경변수 설정, 기본값(|| 기본값 연산자)
+const config = require('../config/config')[env]; // json 객체의 development 불러오기
+const db = {};
+
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
--- prepare/back/routes/post.js
+++ prepare/back/routes/post.js
@@ -13,3 +13,15 @@ router.delete('/', (req, res) => {
 }); // DELETE /post
 
 module.exports = router;
+
+// $npm i sequelize sequelize-cli mysql2
+// $npx sequelize init
+
+/*
+1. homebrew 설치 후
+2. $ brew install mysql
+3. $ brew services start mysql
+4. $ mysql_secure_installation
+5. config/config.js에서 개발모드 username, password, database 설정 - "port": 3306
+6. modules/index.js 에서 sequelize, db 연결
+ */
