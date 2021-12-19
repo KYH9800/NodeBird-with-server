3b3cb99db11f8398b60710699b5b29c0d4fe1573
고윤혁 <sksgur3217@gmail.com>
2021-12-19 23:10:53 +0900

    change a port number

--- prepare/front/config/config.js
+++ prepare/front/config/config.js
@@ -1,3 +1,3 @@
 // eslint-disable-next-line import/prefer-default-export
 export const backURL =
-  process.env.NODE_ENV === 'production' ? 'http://api.coding-factory.site' : 'http://13.124.193.170';
+  process.env.NODE_ENV === 'production' ? 'https://api.coding-factory.site' : 'http://13.124.193.170';
