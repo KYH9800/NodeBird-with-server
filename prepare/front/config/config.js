c39ef75f92c7d42bd24c15583c4db86d8be9f44f
고윤혁 <sksgur3217@gmail.com>
2021-12-20 01:05:37 +0900

    cross origin http >> https

--- prepare/front/config/config.js
+++ prepare/front/config/config.js
@@ -1,3 +1,3 @@
 // eslint-disable-next-line import/prefer-default-export
 export const backURL =
-  process.env.NODE_ENV === 'production' ? 'http://api.coding-factory.site' : 'http://localhost:3065';
+  process.env.NODE_ENV === 'production' ? 'https://api.coding-factory.site' : 'http://localhost:3065';
